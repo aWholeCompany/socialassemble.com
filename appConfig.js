14a95ee65b8492e922b9c2247d7e95b08208e148
Robert Bo Davis <bo@interrobang.consulting>
2020-03-17 17:59:58 -0400

    Fix title/subtitle text

--- appConfig.js
+++ appConfig.js
@@ -1,11 +1,12 @@
 const theme = require('./themes/main.json')
 
 module.exports = {
-  title: 'Gatsby Starter Event Calendar',
-  subTitle: 'The easiest option to share events!',
+  title: 'SocialAssemble',
+  subTitle:
+    'Community based distance learning while you are social distancing.',
   formLink:
     'https://docs.google.com/forms/d/e/1FAIpQLScEoGDqtElfP_RjJIC9kO8nd_5EFv-hUkmCCQzn3R4E-NgWGg',
   maxAmountEvents: 2,
-  limitMonthInTheFuture: 2,
+  limitMonthInTheFuture: 3,
   theme,
 }
