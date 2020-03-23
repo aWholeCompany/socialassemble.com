40398ab4de36777691a68d1d7574cdc45e372500
Robert Bo Davis <bo@interrobang.consulting>
2020-03-23 17:24:22 -0400

    fix: mobile responsive calendar
    fixes #21

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,7 +1,9 @@
 import React, { PureComponent } from 'react'
+import { Box  } from 'grommet'
+import styled from 'styled-components'
 import Layout from '../components/Layout'
 
-const gcal = `<iframe 
+const gcal = `<iframe
 src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=ajVzNDFvYzY0Z2E4c3Z0bWc0bWM3YjZwczhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23795548&amp;showTitle=1&amp;showNav=1"
 style="border:solid 1px #777"
 width="800"
@@ -11,13 +13,20 @@ scrolling="no"
 title="cal">
 </iframe>`;
 
+const Calendar = styled(Box)`
+  position: relative;
+  padding-bottom: 75%;
+  iframe {
+    width: 75vw;
+    height: 75vh;
+  }
+`
+
 class CalendarPage extends PureComponent {
   render() {
     return (
       <Layout>
-        <box align="center" justify="center">
-          <div dangerouslySetInnerHTML={{ __html: gcal }} />
-        </box>
+        <Calendar align="center" justify="center" dangerouslySetInnerHTML={{ __html: gcal }} />
       </Layout>
     )
   }
