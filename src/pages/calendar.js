4bed09dbdc3c564881bffbc45861169ddfebaf5b
Matt Evanoff <matt@homegauge.com>
2020-03-24 16:28:04 -0400

    Changing default to week view

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 import Layout from '../components/Layout'
 
 const gcal = `<iframe
-src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=ajVzNDFvYzY0Z2E4c3Z0bWc0bWM3YjZwczhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23795548&amp;showTitle=1&amp;showNav=1"
+src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=ajVzNDFvYzY0Z2E4c3Z0bWc0bWM3YjZwczhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23795548&amp;showTitle=1&amp;showNav=1&amp;mode=WEEK"
 style="border:solid 1px #777"
 width="800"
 height="600"
